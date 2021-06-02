a2c7b997c1d2084e6a8c3fe944a4a8a6ddc1894b
Ibrahim Abou Elenein <ibrahimabouelanin@gmail.com>
2021-06-02 07:33:47 +0200

    Add convertAddress predicate
    implement convertAddress predicate for direct map.

--- direct-mapping.pl
+++ direct-mapping.pl
@@ -4,3 +4,13 @@ getDataFromCache(_,[item(_,data(N),1,_) | _],Data,HopsNum,directMap):-
 
 getDataFromCache(_,[item(_,_,0,_) | T],Data,HopsNum,directMap):-
     getDataFromCache(_,T,Data,HopsNum,directMap).
+
+pow(_,0,1).
+
+pow(X,Y,Z) :- Y1 is Y - 1,
+              pow(X,Y1,Z1), Z is Z1*X.
+
+convertAddress(Bin,BitsNum,Tag,Idx,directMap):-
+    Tag is Bin // (10**BitsNum),
+    Idx is Bin - (Tag*(10**BitsNum)).
+
