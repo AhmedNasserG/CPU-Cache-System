efa4ed7a9207f460c87e212269bf1b94ab9fd33a
Ibrahim Abou Elenein <ibrahimabouelanin@gmail.com>
2021-06-02 05:58:42 +0200

    Refactor
    remove sekalton variable.

--- direct-mapping.pl
+++ direct-mapping.pl
@@ -1,6 +1,6 @@
-getDataFromCache(_,[item(tag(_),data(N),1,_) | T],Data,HopsNum,directMap):-
+getDataFromCache(_,[item(_,data(N),1,_) | _],Data,HopsNum,directMap):-
     Data is N,
     HopsNum is 0.
 
-getDataFromCache(_,[item(tag(_),data(N),0,_) | T],Data,HopsNum,directMap):-
+getDataFromCache(_,[item(_,_,0,_) | T],Data,HopsNum,directMap):-
     getDataFromCache(_,T,Data,HopsNum,directMap).
