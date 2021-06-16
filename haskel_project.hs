e05e9b543e03073c20e801dc9a476eb39c7c5607
MohammadOTaha <evilix9951@gmail.com>
2021-06-16 17:13:35 +0200

    Add : ReplaceIthItem

--- haskel_project.hs
+++ haskel_project.hs
@@ -4,3 +4,9 @@ convertBinToDecHelper :: (Integral t1, Num p, Num t2) => t1 -> t2 -> p
 convertBinToDecHelper 0 _ = 0
 convertBinToDecHelper x i = 2 ^ mod x 10 + convertBinToDecHelper (div x 10) (i+1)
 
+replaceIthItem x list idx = replaceIthItemHelper x list idx 0
+replaceIthItemHelper :: (Eq t1, Num t1) => t2 -> [t2] -> t1 -> t1 -> [t2]
+replaceIthItemHelper _ [] _ _ = []
+replaceIthItemHelper x (h:t) idx currIdx =
+  if idx == currIdx then x:t
+  else h : replaceIthItemHelper x t idx (currIdx+1)
