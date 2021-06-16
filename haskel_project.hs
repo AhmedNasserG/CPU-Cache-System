478396ddd6b1362ca37ab352637b07f4ef514189
MohammadOTaha <evilix9951@gmail.com>
2021-06-17 00:50:06 +0200

    Add: convertAddress in SetAssociative

--- haskel_project.hs
+++ haskel_project.hs
@@ -1,6 +1,7 @@
 data Item a = It Tag (Data a) Bool Int | NotPresent deriving (Show, Eq)
 data Tag = T Int deriving (Show, Eq)
 data Data a = D a deriving (Show, Eq)
+data Output a = Out (a, Int) | NoOutput deriving (Show, Eq)
 
 convertBinToDec :: Integral a => a -> a
 convertBinToDec a = convertBinToDecHelper a 0
@@ -10,15 +11,26 @@ convertBinToDecHelper x i = if mod x 10 == 1 then (2^i)+ convertBinToDecHelper (
 
 logBase2 :: Floating a => a -> a
 logBase2 = logBase 2
+
 -- getNumBits:: (Floating a1, Integral a, RealFrac a1, Foldable t) => a1 -> [Char] -> t a2 -> a
 getNumBits _ "fullAssoc" _ = 0
 getNumBits numOfBits "setAssoc" _ = logBase2 numOfBits
 getNumBits _ "directMap" lst  = logBase2 ( fromIntegral x) 
       where x = length lst
-replaceIthItem :: (Eq t1, Num t1) => t2 -> [t2] -> t1 -> [t2]
+
+
+replaceIthItem :: (Eq a, Num a) => t -> [t] -> a -> [t]
 replaceIthItem x list idx = replaceIthItemHelper x list idx 0
 replaceIthItemHelper :: (Eq t1, Num t1) => t2 -> [t2] -> t1 -> t1 -> [t2]
 replaceIthItemHelper _ [] _ _ = []
 replaceIthItemHelper x (h:t) idx currIdx =
   if idx == currIdx then x:t
   else h : replaceIthItemHelper x t idx (currIdx+1)
+
+-------------- Set Associative --------------
+-- TODO: expected (Integral b1, Integral b2) => b1 -> b2 -> p -> (b1, b1)
+convertAddress :: (Integral a, Integral b) => a -> b -> [Char] -> (a, a)
+convertAddress binAddress bitsNum "setAssoc" = 
+  (tag,index) where 
+    tag = div binAddress (10^bitsNum)
+    index = mod binAddress (10^bitsNum)
