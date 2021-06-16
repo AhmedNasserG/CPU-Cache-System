0fb7b5c3d2e848cdbe7bb0269c2036e8fae9e2de
Ibrahim Abou Elenein <ibrahimabouelanin@gmail.com>
2021-06-16 22:04:33 +0200

    Add some functions
    impelement logBase2 and getNumBits

--- haskel_project.hs
+++ haskel_project.hs
@@ -1,6 +1,17 @@
+data Item a = It Tag (Data a) Bool Int | NotPresent deriving (Show, Eq)
+data Tag = T Int deriving (Show, Eq)
+data Data a = D a deriving (Show, Eq)
+
 convertBinToDec :: Integral a => a -> a
 convertBinToDec a = convertBinToDecHelper a 0
 convertBinToDecHelper :: (Integral t1, Num p, Num t2) => t1 -> t2 -> p
 convertBinToDecHelper 0 _ = 0
 convertBinToDecHelper x i = 2 ^ mod x 10 + convertBinToDecHelper (div x 10) (i+1)
 
+logBase2 :: Floating a => a -> a
+logBase2 = logBase 2
+-- getNumBits:: (Floating a1, Integral a, RealFrac a1, Foldable t) => a1 -> [Char] -> t a2 -> a
+getNumBits _ "fullAssoc" _ = 0
+getNumBits numOfBits "setAssoc" _ = logBase2 numOfBits
+getNumBits _ "directMap" lst  = logBase2 ( fromIntegral x) 
+      where x = length lst
