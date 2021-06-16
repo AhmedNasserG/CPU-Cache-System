6cf57c5d9c2ae036387a182fae15a2fa8ecf3034
ShimaaBetah <shimaabetah1911@gmail.com>
2021-06-16 19:04:09 +0200

    Fix convertBinToDec

--- haskel_project.hs
+++ haskel_project.hs
@@ -1,6 +1,6 @@
 convertBinToDec :: Integral a => a -> a
 convertBinToDec a = convertBinToDecHelper a 0
-convertBinToDecHelper :: (Integral t1, Num p, Num t2) => t1 -> t2 -> p
+convertBinToDecHelper :: (Integral t1, Integral p, Integral t2) => t1 -> t2 -> p
 convertBinToDecHelper 0 _ = 0
-convertBinToDecHelper x i = 2 ^ mod x 10 + convertBinToDecHelper (div x 10) (i+1)
+convertBinToDecHelper x i = if mod x 10 == 1 then (2^i)+ convertBinToDecHelper (div x 10) (i+1) else convertBinToDecHelper (div x 10) (i+1)
 
